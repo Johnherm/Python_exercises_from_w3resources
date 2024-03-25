5a3486bda5a27db75b49c7143853e1c564cb082c
Yohannes <johnherma72b@gmail.com>
2024-03-25 23:10:27 +0300

    Area

--- Area.py
+++ Area.py
@@ -1,4 +1,5 @@
 from math import pi
+# Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them
 r = int(input("Please enter the value of radius in mm: \n"))
 Area = r*r*pi
 print(f"The area of you entered raduis {r} mm is :\n{Area} mm2\n")
