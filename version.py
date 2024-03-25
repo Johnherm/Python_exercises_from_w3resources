08df9f0968bf396fa104151de161478f22428552
Yohannes <johnherma72b@gmail.com>
2024-03-25 22:40:49 +0300

    python version

---
+++ version.py
@@ -0,0 +1,7 @@
+import sys
+#Write a Python program to find out what version of Python you are using.
+print(" py --vesrion")
+print(" vesrion info.")
+print(" python --vesrion")
+print(sys.version_info)
+print(sys.version)
