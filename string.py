3a1229a52f56d4c5ee2d89d71fda51d8f22303c5
Yohannes <johnherma72b@gmail.com>
2024-03-25 22:23:54 +0300

    first exercises

---
+++ string.py
@@ -0,0 +1,9 @@
+print("Twinkle, twinkle, little, star")
+print("\tHow I wonder what you are!")
+print("\t\tUp above the world so high!,")
+print("\t\tLike a diamond in the sky,")
+print("Twinkle, twinkle, little, star")
+print("\tHow I wonder what you are!")
+# Or we can write as follow
+print("\n\n\n\n")
+print("Twinkle, twinkle, little, star\n\tHow I wonder what you are!\n\t\tUp above the world so high!,\n\t\tLike a diamondint the sky.\nTwinkle, twinkle, little, star,\n\tHow I wonder whay you are!")
